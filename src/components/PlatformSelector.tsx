94575715d2f5415bebd72e2df0f6defc46a73788
Martin Fallenstedt <martin.fallenstedt@gmail.com>
2023-04-29 15:27:25 -0400

    Filter games by platform

---
+++ src/components/PlatformSelector.tsx
@@ -0,0 +1,34 @@
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { BsChevronDown } from "react-icons/bs";
+import usePlatforms, { Platform } from "../hooks/usePlatform";
+
+interface Props {
+    selectedPlatform: Platform | null;
+    onSelectedPlatform: (platform: Platform) => void;
+}
+
+const PlatformSelector = ({ selectedPlatform, onSelectedPlatform }: Props) => {
+    const { data, error } = usePlatforms();
+
+    if (error) return null;
+
+    return (
+        <Menu>
+            <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+                {selectedPlatform?.name || "Platform"}
+            </MenuButton>
+            <MenuList>
+                {data.map((platform) => (
+                    <MenuItem
+                        key={platform.id}
+                        onClick={() => onSelectedPlatform(platform)}
+                    >
+                        {platform.name}
+                    </MenuItem>
+                ))}
+            </MenuList>
+        </Menu>
+    );
+};
+
+export default PlatformSelector;
