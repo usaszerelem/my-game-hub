9beb2e38282939f63e8b20cccbc1c2df7a37d2e3
Martin Fallenstedt <martin.fallenstedt@gmail.com>
2023-05-01 17:57:02 -0400

    Ship genres and platform static data with app

--- src/hooks/usePlatform.ts
+++ src/hooks/usePlatform.ts
@@ -1,13 +1,16 @@
-import useData from "./useData";
+//import useData from "./useData";
+import platforms from '../data/platforms';
 
 export interface Platform {
     id: number;
     name: string;
     slug: string;
 }
-
+/*
 const usePlatforms = () => {
     return useData<Platform>('/platforms/lists/parents');
 };
+*/
 
+const usePlatforms = () => ({data: platforms, isLoading: false, error: null});
 export default usePlatforms;
