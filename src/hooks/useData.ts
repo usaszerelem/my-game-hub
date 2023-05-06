2d219553138dbe6da13f192a273908eae7196d0a
Martin Fallenstedt <martin.fallenstedt@gmail.com>
2023-05-06 15:15:28 -0400

    Added react-query to retreive Genres

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
+import apiClient from "../services/apiClient";
 import { AxiosRequestConfig, CanceledError } from "axios";
 
-interface FetchResponse<T> {
+export interface FetchResponse<T> {
     count: number;
     results: T[];
 }
